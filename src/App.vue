b5d030cacede85debccb59a1404cb0ca7bca30d6
Matthias <matthias.heiden.mh@gmail.com>
2018-12-09 19:53:35 +0100

    removed presets

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-
-    <div class="container">
-      <h1> Hello World</h1>
-    </div>
-  </div>
+  <div id="app"></div>
 </template>
 
+
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+  components: {}
 })
 export default class App extends Vue {}
 </script>
 
+
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
